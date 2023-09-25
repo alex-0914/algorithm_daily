cb5d79b56a4f536a38e72ec31cdb352b68968ecf
alex-0914 <alextangting@gmail.com>
2023-09-25 22:37:12 +0800

    project init

---
+++ src/main/java/com/example/algorithm_daily/AlgorithmDailyApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithm_daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmDailyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmDailyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/algorithm_daily/AlgorithmDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithm_daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmDailyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
